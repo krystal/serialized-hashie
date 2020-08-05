f92b76eed39c33889ad7a7a0b1fd1ca733bf69b3
Adam Cooke <me@adamcooke.io>
2020-08-05 12:02:52 +0100

    support for auto versionining when making tags

--- lib/serialized_hashie/version.rb
+++ lib/serialized_hashie/version.rb
@@ -2,6 +2,11 @@
 
 module SerializedHashie
 
-  VERSION = '1.0.3'
+  VERSION_FILE_ROOT = File.expand_path('../../VERSION', __dir__)
+  if File.file?(VERSION_FILE_ROOT)
+    VERSION = File.read(VERSION_FILE_ROOT).strip.delete_prefix('v')
+  else
+    VERSION = '0.0.0.dev'
+  end
 
 end
